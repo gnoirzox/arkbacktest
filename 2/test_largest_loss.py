7d6d3fef0746b66844531ad26d3d41a3092e2c04
Simon ROUGER <simon.rouger@supinfo.com>
2019-11-04 01:53:03 +0000

    fixes of largestLoss unit tests

--- 2/test_largest_loss.py
+++ 2/test_largest_loss.py
@@ -8,24 +8,14 @@ class TestLargestLoss(TestCase):
         self.assertEqual(largestLoss(prices_list), 1230)
 
     def test_largest_loss_function_empty_raise(self):
-        with self.assertRaises(Exception) as context:
-            largestLoss([])
-
-        self.assertTrue('The submitted list is empty' in context.exception)
+        self.assertRaises(Exception, lambda: largestLoss([]))
 
     def test_largest_loss_function_negative_raise(self):
         prices_list = [0, 12, -4, 1234]
 
-        with self.assertRaises(Exception) as context:
-            largestLoss(prices_list)
-
-        self.assertTrue('The prices cannot be negative' in context.exception)
+        self.assertRaises(Exception, lambda: largestLoss(prices_list))
 
     def test_largest_loss_function_odd_raise(self):
         prices_list = [0, 12, 1234]
 
-        with self.assertRaises(Exception) as context:
-            largestLoss(prices_list)
-
-        self.assertTrue('The length of the submitted list needs to be even ' \
-            'in order to check both the related bought and sold prices' in context.exception)
+        self.assertRaises(Exception, lambda: largestLoss(prices_list))
